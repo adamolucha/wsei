b82d7187229e63cf26c5bb1d240fb9bf4c86ffed
adamolucha <adam.olucha@microsoft.wsei.edu.pl>
2022-12-03 17:01:14 +0100

    emerytura add

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // tu wpisujesz kod
+        string nazwisko = Console.ReadLine();
+        int wiek = int.Parse(Console.ReadLine());
+        int wiek_emerytalny = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Witaj, {nazwisko}!");
+        if (wiek < 0 && wiek_emerytalny <= 0)
+        {
+            Console.WriteLine("Wiek nie może być ujemny!");
+        }
+        if (wiek < 0 && wiek_emerytalny >= 0)
+        {
+            Console.WriteLine($"Wiek nie może być ujemny!");
+        }
+        if (wiek >= 0 && wiek_emerytalny < 0)
+        {
+            Console.WriteLine($"Wiek nie może być ujemny!");
+        }
+        if (wiek < wiek_emerytalny && wiek >= 0 && wiek_emerytalny > 0)
+        {
+            Console.WriteLine($"Do emerytury brakuje Ci {wiek_emerytalny - wiek} lat!");
+        }
+        if (wiek >= wiek_emerytalny && wiek > 0 && wiek_emerytalny >= 0)
+        {
+            Console.WriteLine("Jesteś emerytem!");
+        }
+
+    }
+}
+
+
